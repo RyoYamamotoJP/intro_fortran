a21e03748a256c5fb78a72438f5bc62552994928
Ryo Yamamoto <email@ryoyamamoto.jp>
2019-05-02 17:48:34 +0900

    Add repeating3

---
+++ repeating3.f90
@@ -0,0 +1,12 @@
+program repeating3
+  implicit none
+  real(8) :: x = 1, x0 = 0, a = 10
+  integer :: c = 0
+  do while (abs(x - x0) > spacing(x0)*2)
+    c = c + 1
+    x0 = x
+    x = (x + a/x)/2
+    print *, c, x
+  end do
+end program repeating3
+
