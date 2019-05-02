6ed66aa578054d07eeb52e1dd180b569bcb509dd
Ryo Yamamoto <email@ryoyamamoto.jp>
2019-05-02 22:43:17 +0900

    Add g function to new_func program

--- new_func.f90
+++ new_func.f90
@@ -1,3 +1,13 @@
+function g(x)
+  implicit none
+  real(8) g, x
+  if (abs(x) < 1) then
+    g = sqrt(1 - x**2)
+  else
+    g = sqrt(x**2 - 1)
+  end if
+end function g
+
 function f(x)
   implicit none
   real(8) f, x
@@ -6,11 +16,10 @@ end function f
 
 program new_func
   implicit none
-  real(8) x, y, f
-  x = -1.5
-  do while (x < 1.5)
-    y = f(x)
-    print *, x, y
+  real(8) x, f, g
+  x = -1.5d0
+  do while (x < 1.5d0)
+    print *, x, f(x), g(x)
     x = x + 0.1d0
   end do
 end program new_func
