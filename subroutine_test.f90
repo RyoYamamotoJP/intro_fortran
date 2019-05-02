537faa644867db58a5e14629bb61fbc03beb7862
Ryo Yamamoto <email@ryoyamamoto.jp>
2019-05-02 11:26:46 +0900

    Add subroutine_test

---
+++ subroutine_test.f90
@@ -0,0 +1,18 @@
+program subroutine_test
+  call english
+  call spanish
+  call french
+end program subroutine_test
+
+subroutine english
+  print *, 'How are you?'
+end subroutine english
+
+subroutine spanish
+  print *, '?Commo esta usted?'
+end subroutine spanish
+
+subroutine french
+  print *, 'Comment allez-vous?'
+end subroutine french
+
